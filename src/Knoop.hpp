067d4b8c26ee17f00a85acdc8ee72900c6137d99
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-05-31 12:22:21 -0600

    Adding first unit test.

---
+++ src/Knoop.hpp
@@ -0,0 +1,8 @@
+#ifndef KNOOP_HPP
+#define KNOOP_HPP
+
+namespace knoop {
+  #include "Knoop/Node.hpp"
+}
+
+#endif // KNOOP_HPP
---
+++ src/Knoop/Node/test/Node.test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+#include "Knoop.hpp"
+
+SCENARIO( "testing the Node class" ){
+} // SCENARIO
