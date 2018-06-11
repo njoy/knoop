40571f203e24f05ca49459ca0b47edc53995cfb5
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-06-11 08:16:57 -0600

    Adding a few additional examples.

--- src/test/example1.cpp
+++ src/test/example1.cpp
@@ -1,4 +1,33 @@
 
+#include <iostream>
+#include "knoop.hpp"
+
+using namespace njoy::knoop;
+using std::cout;
+using std::endl;
+
+void nesting();
+
 int main( ){
+  // We can create a node that takes only a single type
+  Node<double> dNode{1.1};
+  Node<int> iNode{2};
+  Node< std::string > sNode{ "three" };
+
+  // The leaf values of those nodes can be extracted and printed
+  cout << "dNode has value: " << dNode.get< double >() << endl;
+  cout << "iNode has value: " << iNode.get< int >() << endl;
+  cout << "sNode has value: " << sNode.get< std::string >() << endl;
+
+  // We can create a Node type that can take a variety of different leaf types
+  using Node_t = Node< double, int, std::string >;
+  Node_t varNode{1.1, 2, "three"};
+  // cout << "varNode has double value: " << varNode.get< double >() << endl;
+  // cout << "varNode has int value: " << varNode.get< int >() << endl;
+  // cout << "varNode has string value: " << varNode.get< std::string >() << endl;
+
+  // Example of how to create nested nodes
+  nesting();
+
   return 0;
 }
---
+++ src/test/nestedExample.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+#include "knoop.hpp"
+
+using namespace njoy::knoop;
+using std::cout;
+using std::endl;
+
+void nesting(){
+
+  cout << "\nNesting example" << endl;
+
+  using Node_t = Node< double, int, std::string >;
+
+  // Create a "map" type of node
+  auto mNode = Node_t::makeMap();
+
+  // Add some child nodes
+  mNode.insert( "double", 1.1 );
+  mNode.insert( "int", 2 ).insert( "string", "three" );
+
+  mNode.insert( "child", Node_t::makeMap() );
+  // auto child = mNode.get< std::map >();
+}
