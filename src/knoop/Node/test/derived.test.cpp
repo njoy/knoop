486fc2817450779810adc21891563cdecac9cb60
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-11-13 12:28:59 -0700

    Fixing up some variant stuff.

---
+++ src/knoop/Node/test/derived.test.cpp
@@ -0,0 +1,10 @@
+#include "knoop.hpp"
+#include "catch.hpp"
+
+using namespace njoy::knoop;
+using Node_t = Node<int, std::string>;
+
+SCENARIO( "testing derived inheritance" ){
+  
+} // SCENARIO
+
